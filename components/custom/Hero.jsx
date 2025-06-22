b6b75c1568e5544b68144db879df03cf8cf8f1e6
Shalabh sahu <sahushalabh2001@gmail.com>
2025-06-22 20:16:47 +0530

    landing page completed

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Image from 'next/image'
 
 function Hero() {
   return (
     <div className='px-10 md:px-28 lg:px-44 xl:px-56 flex flex-col items-center mt-24'>
-      <h2 className='font-extrabold text-5xl'>
-        AI-Powered Email Templates
+      <h2 className='font-extrabold text-5xl text-center'>
+        AI-Powered 
+        <span className='text-primary'>
+          Email Templates
+        </span>
       </h2>
+      <p className='text-center mt-4'>Longing to impress clients with AI Powered emails but do not have time to build them on your own
+        ? Use AI Powered email templates that already have AI Generated images and copy -- save time on email
+        Production with us.
+      </p>
+      <div className='flex gap-5 mt-6'>
+        <Button variant="outline">Try Demo</Button>
+        <Button>Get Started</Button>
+      </div>
+      <Image src={'/landing.png'} alt='landing' 
+      width={1000} height={800}
+      className='mt-1 rounded-xl'/>
     </div>
   )
 }
